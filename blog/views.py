ea0bb24560d979b90cc2c92af5c59873a54d739b
Дмитрий Волков <roskey@mail.ru>
2025-06-14 20:24:28 +0300

    Доработал view для пагинации.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from django.views.generic import ListView, DetailView
 from django.shortcuts import get_object_or_404
 
@@ -6,20 +7,37 @@ from blog.models import Post
 
 
 class PostListView(ListView):
+    """
+    Для отображения списка постов в блоге.
+    """
     queryset = Post.published.all()             # Используем только опубликованные посты
     context_object_name = 'posts'               # В шаблоне будет - {% for post in posts %} (список постов).
     template_name = 'blog/blog.html'
-    extra_context = {
-        'title': 'Блог'
-    }
+    paginate_by = 2
+
+    def get_context_data(self, **kwargs):
+        """
+        Это один из вариантов extra_context.
+        Так можно передать множество параметров. Этот метод более гибкий, чем extra_content
+
+        :param kwargs:
+        :return: values
+        """
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Блог'
+        return context
 
 
 # def post_list(request):                       # удалить, если BlogListView написан корректно
-#     posts = Post.published.all()
+#     post_list_ = Post.published.all()
+#     # Постраничная разбивка с 3 постами на страницу
+#     paginator = Paginator(post_list_, 3)
+#     page_number = request.GET.get('page', 1)
+#     posts = paginator.page(page_number)
 #     return render(
-#         request=request,
-#         template_name='blog/post/list.html',
-#         context={'posts': posts}
+#         request,
+#         'blog/post/list.html',
+#         {'posts': posts}
 #     )
 
 
@@ -47,14 +65,17 @@ class PostDetailView(DetailView):
         )
 
 
-# def post_detail(request, id):                   # удалить, если PostDetailView написан корректно
+# def post_detail(request, year, month, day, post):                   # удалить, если PostDetailView написан корректно
 #     post = get_object_or_404(
 #         Post,
-#         id=id,
-#         status=Post.Status.PUBLISHED
+#         status=Post.Status.PUBLISHED,
+#         slug=post,
+#         publish__year=year,
+#         publish__month=month,
+#         publish__day=day
 #     )
 #     return render(
 #         request,
 #         'blog/post/detail.html',
 #         {'post': post}
-#         )
+#     )
